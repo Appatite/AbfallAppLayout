24bf8ffdbdbeeb70fc5905c5b6ce780d87db6c1a
micha <micha@micharechner.(none)>
2015-04-04 22:38:10 +0200

    Zweite Variante Arbeit Arbeitsrecht

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 
 
 
-window.addEventListener('resize', function(event){
+/*window.addEventListener('resize', function(event){
   var height = window.innerHeight;
   //alert(height);
     var wrapper = document.getElementById("wrapper");
@@ -20,14 +20,102 @@ var height = window.innerHeight;
     var wrapper = document.getElementById("wrapper");
   wrapper.style.height = height + "px";
 
+}*/
+
+function navReset(element){
+	element.setAttribute("class", "displayblock");
 }
 
-function menuFunction(idname){
-	var inner = document.getElementById(idname);
-	inner.setAttribute("class", "displaynone");
+function disclaimerFunction(){
+	
+	var disclaimer= document.getElementById("disclaimer");
 	var nav = document.getElementById("nav");
-	nav.setAttribute("class", "displayblock");
+	var navdisplay = nav.getAttribute("class");
+
+	if(navdisplay == "displaynone") {
+			disclaimer.setAttribute("class", "displayblock");
+			
+		}
+		
+		else if (navdisplay == "displayblock") {
+
+		nav.setAttribute("class", "displaynone");
+		
+		window.setInterval("navReset(nav)", 50);
+
+		
+	}
+	
+
+
 }
+
+function disclaimerHideFunction(){
+	var nav = document.getElementById("nav");
+	var navdisplay = nav.getAttribute("class");
+	
+	if(navdisplay == "displaynone") {
+	
+	var disclaimer= document.getElementById("disclaimer");
+	disclaimer.setAttribute("class", "displaynone");
+	var inner = document.getElementById("inner");
+	inner.setAttribute("class", "displaynone");
+	
+
+	
+	
+	
+	nav.setAttribute("class", "displayblock");
+	
+	var wrapperStart = document.getElementById("wrapperStart");
+	wrapperStart.style.backgroundColor = "white";
+	wrapperStart.style.marginTop= "80px";
+	wrapperStart.style.position = "fixed";
+	wrapperStart.style.overflow = "scroll";
+	}
+	
+	
+	
+	
+}
+
+
+function menuFunction(idname){
+	//alert(idname);
+	var inner = document.getElementById(idname);
+	var nav = document.getElementById("nav");
+	inner.setAttribute("class", "displaynone");
+	var navdisplay = nav.getAttribute("class");
+	
+	//nav.setAttribute("class", "displayblock");
+	
+
+	
+	if (navdisplay == "displayblock") {
+
+		nav.setAttribute("class", "displaynone");
+		
+		window.setInterval("navReset(nav)", 50);}
+		
+		else if (navdisplay == "displaynone"){
+			nav.setAttribute("class", "displayblock");}
+	
+	var wrapperStart = document.getElementById("wrapperStart");
+	wrapperStart.style.backgroundColor = "white";
+	wrapperStart.style.marginTop= "80px";
+	wrapperStart.style.position = "fixed";
+	wrapperStart.style.overflow = "scroll";
+}
+
+/*function linkeinsOben(){
+	
+	/*var wrapperStart = document.getElementById("wrapperStart");
+	wrapperStart.style.top = "-300px";*/
+	
+	/*var linkeinsoben = document.getElementById("linkeins");
+	linkeinsoben.style
+}*/
+
 
 
 function endApp(){
